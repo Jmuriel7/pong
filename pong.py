7cc79a4ca4b3346db4498ea426f26506af15b8fb
JoseMuriel <jmr_snake@hotmail.com>
2025-03-16 19:57:43 +0000

    quitamos los números mágicos

--- pong.py
+++ pong.py
@@ -1,23 +1,22 @@
 import pygame
 
+# Configuraciones
+ANCHO = 640
+ALTO = 480
+COLOR_FONDO = (0, 50, 0)
+
 class Pong:
 
     def __init__(self):
         pygame.init()
-        self.pantalla = pygame.display.set_mode((640, 480))
+        self.pantalla = pygame.display.set_mode((ANCHO, ALTO))
 
     def jugar(self):
-        pantalla = self.pantalla
-
         salir = False
-
-        pos_x = 0
-        pos_y = 0
 
         while not salir:
             # Bucle principal (main loop)
-            pos_x += 1
-            pos_y += 1
+            
             # Capturar los eventos
             for evento in pygame.event.get():
                 if evento.type == pygame.QUIT:
@@ -26,16 +25,19 @@ class Pong:
     
         # renderizar mis objetos (pintar en pantalla)
         # 1. borrar pantalla
-        pygame.draw.rect(self.pantalla, (0, 0, 50), ((0,0), (640,480)))
+        pygame.draw.rect(
+            self.pantalla,
+            COLOR_FONDO,
+            ((0,0), (ANCHO, ALTO)))
 
         # 2. Pintar los objetos en la posición correspondiente
-        rectangulo = pygame.Rect(pos_x / 10, pos_y / 20, 250, 150)
+        rectangulo = pygame.Rect(ANCHO/2 - 125, ALTO/2 - 75, 250, 150)
         pygame.draw.rect(self.pantalla, (10, 68, 158), rectangulo)
 
         # 3. mostrar los cambios en la pantalla
         pygame.display.flip()
 
-pygame.quit()
+    pygame.quit()
 
 
 juego = Pong()
